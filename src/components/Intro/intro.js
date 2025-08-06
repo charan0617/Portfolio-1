ef77a05e8929b956ae23717d72e879d282e55cdf
charan k <charankathirvel17@gmail.com>
2025-08-06 19:04:52 +0530

    second commit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -4,7 +4,6 @@ import bg from '../../accets/ck.png'
 import btnImg from '../../accets/hireme.png'
 import {Link} from 'react-scroll'
 
-
 const Intro = () => {
   return (
     <section id="intro">
@@ -12,7 +11,7 @@ const Intro = () => {
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Charan</span> <br />Web Developer</span>
             <p className="introPara">I am a skilled web developer with experience in creating <br />visually appealing and user friendy websites.</p>
-            <Link><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg'/>Hire me</button></Link>
+            <Link to='contact' smooth={true} duration={500}><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg'/>Hire me</button></Link>
         </div>
         <img src={bg} alt="Profile" className="bg" />
     </section>
